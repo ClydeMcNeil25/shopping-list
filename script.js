0cc673da2bc450c7014d6c6e66c2f1ce61ddc90f
Clyde McNeil <cmcneil2@cnm.edu>
2022-07-01 15:34:45 -0500

    Phase 2 Finished

--- script.js
+++ script.js
@@ -3,22 +3,20 @@ window.onload = function () {
 };
 
 function initShoppingList() {
-    let form = document.getElementById("item-form")
-    console.log(form);
+    let form = document.getElementById("item-form");
 
     form.addEventListener("submit", (event) => {
-        handleItemForm(e, form);
+        handleItemForm(event, form);
     });
 }
 
 function handleItemForm(event, formRef) {
-    if(event.preventDefault()) {
+    if(event.preventDefault) {
         event.preventDefault();
     }
 
-    console.log("event: ", event);
-
-addItemToShoppingList();
+    addItemToShoppingList();
+    formRef.reset();
 
     return false;
 }
@@ -26,15 +24,37 @@ addItemToShoppingList();
 function addItemToShoppingList() {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
-    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+    let id = getRandomInt(0, 10000000);
 
-    console.log("Item HTML: ", itemHtml);
+    //Creates list item and appends to page.
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
     let itemListRef = document.getElementById("shopping-list");
     itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+    setDeleteButtonEvent(id);
 }
 
-function createListItemHtml(itemName, itemAmount) {
-    return `<li>
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button"+id);
+    deleteButton.addEventListener("click", () => {
+        removeListItem(id);
+    });
+}
+//Lists the item and amount and houses the delete button.
+function createListItemHtml(itemName, itemAmount, id) {
+    return `<li id="item${id}">
                 ${itemName} - ${itemAmount}
+                <button id="button${id}">Delete</button>
             </li>`;
+}
+
+function removeListItem(id) {
+    let listItem = document.getElementById("item"+id);
+    listItem.parentNode.removeChild(listItem);
+}
+//Adds the minimum amount for the item listed in the form.
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
